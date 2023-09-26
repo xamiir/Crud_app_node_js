2628436b54968ff48cd17b479c037e4a36fed227
Eng abdalla <cllaahicabdi04@gmail.com>
2023-09-26 10:40:29 +0300

    upload_imsge

--- routes/routes.js
+++ routes/routes.js
@@ -2,44 +2,34 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/users");
 const multer = require("multer");
-const e = require("express");
+const fs = require("fs");
 
-// image upload
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./uploads");
+// upload image
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./uploads/");
   },
 
-  filename: function (req, file, cb) {
-    cb(null, file.fieldname + "-" + Date.now() + "-" + file.originalname);
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + file.originalname);
   },
 });
 
-var upload = multer({ storage: storage }).single("image");
+const upload = multer({ storage: storage });
 
-// insert user database
-router.post("/add", upload, (req, res) => {
-  const user = new User({
+// add user
+router.post("/add", upload.single("image"), (req, res) => {
+  let user = new User({
     name: req.body.name,
     email: req.body.email,
     phone: req.body.phone,
     image: req.file.filename,
   });
 
-  // user.save((err) => {
-  //   if (err) {
-  //     res.json({ message: err.message, type: "danger" });
-  //   } else {
-  //     req.session.message = {
-  //       type: "success",
-  //       message: "User added successfully",
-  //     };
-  //     res.redirect("/");
-  //   }
-  // });
   user
     .save()
-    .then((data) => {
+    .then(() => {
       req.session.message = {
         type: "success",
         message: "User added successfully",
@@ -47,7 +37,7 @@ router.post("/add", upload, (req, res) => {
       res.redirect("/");
     })
     .catch((err) => {
-      res.json({ message: err.message, type: "danger" });
+      console.log(err);
     });
 });
 
@@ -65,4 +55,24 @@ router.get("/add", (req, res) => {
   res.render("add-users", { title: "Add users" });
 });
 
+router.get("/delete/:id", async (req, res) => {
+  let id = req.params.id;
+  try {
+    let user = await User.findByIdAndRemove(id);
+    if (user.image != "") {
+      try {
+        fs.unlinkSync("./uploads/" + user.image);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    req.session.message = {
+      type: "info",
+      message: "User deleted successfully",
+    };
+    res.redirect("/");
+  } catch (err) {
+    res.json({ message: err.message, type: "danger" });
+  }
+});
 module.exports = router;
